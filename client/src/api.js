632702352736df9d347fe62fe0044c96b1d585a1
starstarrr <maxingran23@163.com>
2026-07-20 18:49:32 -0700

    use relative API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,11 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ||
-  "http://localhost:3000";
+  import.meta.env.VITE_API_BASE_URL || "";
 
 export async function apiRequest(path, options = {}) {
   const response = await fetch(
     `${API_BASE_URL}${path}`,
     {
       ...options,
-
       headers: {
         "Content-Type": "application/json",
         ...(options.headers || {})
